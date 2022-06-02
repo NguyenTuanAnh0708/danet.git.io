86872b36e5a8c53f76e2c134cc40d9a6f6c58781
teo <tuananhnguyenbsbd@gmail.com>
2022-06-02 14:36:56 +0700

    ok

--- asset/js/js.js
+++ asset/js/js.js
@@ -18,40 +18,29 @@ const next=()=>{
         setTimeout(add(i),5000)
     }
 }
-$(document).ready(function(){
-    $('.carosel-all').slick({
-        slidesToShow: 6,
-        slidesToScroll: 3,
-        prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
-        nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
-        
-        responsive: [
-            {
-              breakpoint: 1024,
-              settings: {
-                slidesToShow: 6,
-                slidesToScroll: 3,
-              }
-            },
-            {
-              breakpoint: 600,
-              settings: {
-                slidesToShow: 6,
-                slidesToScroll: 3,
-              }
-            },
-            {
-              breakpoint: 480,
-              settings: {
-                slidesToShow: 3,
-                slidesToScroll: 2,
-              }
-            }
-            // You can unslick at a given breakpoint now by adding:
-            // settings: "unslick"
-            // instead of a settings object
-          ]
-    }
 
-);
+$(document).ready(function(){
+  $('.carosel-all').slick({
+    slidesToShow: 6,
+    slidesToScroll: 3,
+    prevArrow: '<div class="slick-prev"><i class="fa fa-angle-left" aria-hidden="true"></i></div>',
+    nextArrow: '<div class="slick-next"><i class="fa fa-angle-right" aria-hidden="true"></i></div>',
+    responsive: [
+      {
+        breakpoint: 740,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow:3,
+          slidesToScroll:1,
+        }
+      }
+    ]
+
   });
+});
